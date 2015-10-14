1fe9b126083d7daf9b9a9631c581055a9a816d02
Jared Whitney <jaredwhitney3@gmail.com>
2015-10-13 20:21:55 -0400

    Drive train finished(i think)

---
+++ src/org/robockets/tshirtcannon/RobotMap (2).java
@@ -0,0 +1,13 @@
+package org.robockets.tshirtcannon;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	
+	public RobotMap() {
+	}
+}
