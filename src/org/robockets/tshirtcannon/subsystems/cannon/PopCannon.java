7f387e660332396881779166a63342214465e4f1
Jake Kinsella <jake.kinsella@gmail.com>
2015-11-19 20:04:30 -0500

    Fix terrible if statements, fix styling of constants, move constant variable declaraction

--- src/org/robockets/tshirtcannon/subsystems/cannon/PopCannon.java
+++ src/org/robockets/tshirtcannon/subsystems/cannon/PopCannon.java
@@ -19,8 +19,14 @@ public class PopCannon extends Command {
     
     // Called just before this Command runs the first time
     protected void initialize() {
-    	if(!cannon.PID) end(); // If it's unaligned, I will not pop!
-        if(RobotMap.safetySensor.getDistance() < 200 && RobotMap.safetyEnabled == true) end(); //Something within 2 meters, I will not pop!
+    	if (!cannon.PID) {
+    		end(); // If it's unaligned, I will not pop!
+    	}
+    	
+        if (RobotMap.safetySensor.getDistance() < 200 && RobotMap.safetyEnabled == true) {
+        	end(); //Something within 2 meters, I will not pop!
+        }
+        
     	setTimeout(timeout);
     }
 
@@ -42,6 +48,6 @@ public class PopCannon extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	cannon.setValve(false);
+    	end();
     }
 }
