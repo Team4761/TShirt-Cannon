55cf5dc173d391e5d15623238580fedc3e656e80
jakebacker <jbacker42@gmail.com>
2015-12-03 20:27:02 -0500

    Change to use RobotMap as reference

--- src/org/robockets/tshirtcannon/subsystems/DriveTrain.java
+++ src/org/robockets/tshirtcannon/subsystems/DriveTrain.java
@@ -3,9 +3,7 @@ package org.robockets.tshirtcannon.subsystems;
 import org.robockets.tshirtcannon.RobotMap;
 import org.robockets.tshirtcannon.commands.Drive;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -13,7 +11,7 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class DriveTrain extends Subsystem {
 	
-	public static final RobotDrive drivetrain = new RobotDrive(1,2,3,4);
+	public static final RobotDrive drivetrain = new RobotDrive(RobotMap.frontLeftMotor,RobotMap.backLeftMotor,RobotMap.frontRightMotor,RobotMap.backRightMotor);
 
     public void initDefaultCommand() {
     	setDefaultCommand(new Drive());
