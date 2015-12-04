e91f5efa25cb2b5bc18f4dd1a7ec6c213819163b
jakebacker <jbacker42@gmail.com>
2015-12-03 20:24:51 -0500

    Remove unnecesary comments

--- src/org/robockets/tshirtcannon/commands/Drive.java
+++ src/org/robockets/tshirtcannon/commands/Drive.java
@@ -4,9 +4,6 @@ import org.robockets.tshirtcannon.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-/**
- *
- */
 public class Drive extends Command {
 
     public Drive() {
@@ -14,28 +11,22 @@ public class Drive extends Command {
     	requires(Robot.drivetrain);
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
     }
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
 
     	Robot.drivetrain.takeJoystickInputs(Robot.oi.joyStick.getRawAxis(0), Robot.oi.joyStick.getRawAxis(1));
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return false;
     }
 
-    // Called once after isFinished returns true
     protected void end() {
     	Robot.drivetrain.stop();
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     	end();
     }
